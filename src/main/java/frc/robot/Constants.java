0a5ff723d18a27b5dfb9b1d105fc61a2eccdeb96
Tigerbytes 1775 Team Account <tigerbytes1775@gmail.com>
2026-02-11 16:35:38 -0600

    add isblue

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,11 @@
 
 package frc.robot;
 
+import java.util.Optional;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,9 +18,42 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+  public static boolean isBlue = DriverStation.getAlliance().get() == Alliance.Blue;
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
 
     public static final double triggerDeadband = 0.1;
+  }
+
+  public static class Targets {
+
+    public static final double[] hub;
+    public static final double[] leftShot;
+    public static final double[] rightShot;
+    public static final double[] farShot;
+
+    static {
+      if (isBlue) {
+        hub = new double[] {4.035,4.03,1.1938};
+        leftShot = new double[] {2,2,0};
+        rightShot = new double[] {6,2,0};
+        farShot = new double[] {4.035,2,0};
+      } else {
+        hub = new double[] {4.035,4.03,1.1938};
+        leftShot = new double[] {2,2,0};
+        rightShot = new double[] {6,2,0};
+        farShot = new double[] {4.035,2,0};
+      }
+
+
+    }
+    
+    
+  
+
+    
+
   }
 }
